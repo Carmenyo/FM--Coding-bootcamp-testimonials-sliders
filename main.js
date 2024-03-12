0aa25d13e8f70dd0baabcf13d124640309c5fa15
Carmen Castro <carmencastro@MacBook-Air-de-Carmen.local>
2024-03-12 11:41:26 +0100

    Readme.md

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Mostrar la diapositiva de Tanya al cargar la página
-    showSlide("testimonial1");
-  
-    // Obtener los elementos de navegación
-    var prevButton = document.querySelector(".prev");
-    var nextButton = document.querySelector(".next");
-  
-    // Agregar event listeners a los botones de navegación
-    prevButton.addEventListener("click", function () {
-      showSlide("testimonial1");
-    });
-  
-    nextButton.addEventListener("click", function () {
-      showSlide("testimonial2");
-    });
-  });
-  
-  function showSlide(slideId) {
-    // Ocultar todas las diapositivas
-    var slides = document.querySelectorAll(".testimonial");
-    slides.forEach(function (slide) {
-      slide.style.display = "none";
-    });
-  
-    // Mostrar la diapositiva específica
-    var currentSlide = document.getElementById(slideId);
-    if (currentSlide) {
-      currentSlide.style.display = "block";
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const image = document.querySelector('.image');
+const feedback = document.querySelector('.feedback');
+
+const users = [
+    {
+        name: 'Tanya Sinclair',
+        role: 'UX Engineer',
+        feedback: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
+        image: './images/tanya.svg'
+    },
+    {
+        name: 'John Tarkpor',
+        role: 'Junior Front-end Developer',
+        feedback: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
+        image: './images/john.svg'
     }
-  }
-  
+]
+
+let slide = 0;
+
+const navigation = () => {
+    if(slide > users.length-1){
+        slide = 0;
+    }
+    image.innerHTML = `
+    <img src="${users[slide].image}" alt="img">
+    `
+    feedback.innerHTML = `
+    <img src="./images/pattern-quotes.svg" alt="quotes">
+    <p>${users[slide].feedback}</p>
+    <h4 class="name">${users[slide].name} <span class="role">${users[slide].role}</span></h4>
+    `;
+    slide++;
+}
+
+navigation();
+
+prevBtn.addEventListener('click', navigation);
+nextBtn.addEventListener('click', navigation);
   
